aa3ec619a657a792c3ec2d8196b7500f7fdb1ffb
mayomi1 <superdelaj@gmail.com>
2017-05-25 16:15:21 +0100

    favorite tweet function added

--- bot.js
+++ bot.js
@@ -6,12 +6,12 @@ var Twitter  = new twit(config);
 
 vat retweet = function(){
   var params = {
-    q: '#nodejs', '#Nodejs',
+    q: '#nodejs, #Nodejs',
     reqult_type: 'recent',
     lang: 'en'
   }
 
-  Twitter.get('search/tweets', params, function(err, data{
+  Twitter.get('search/tweets', params, function(err, data){
     //if there no error
     if(!err){
       //grab ID of tweet to retweet
@@ -31,10 +31,56 @@ vat retweet = function(){
     }
     //if unable to search a tweet
     else{console.log('Something went wrong while searching');}
-  }));
+  });
 }
 
 //grab and retweet
 retweet();
 //retweet in every 50 mins
 setInterval(retweet, 3000000)
+
+
+
+//favorite bot
+
+//find a randonm tweet and favorite it
+var favoriteTweet = function(){
+  var params = {
+    q: '#nodejs, #Noodejs',
+    result_type: 'recent',
+    lang: 'en'
+  }
+
+  //find the tweet
+  Twitter.get('search/tweets', params, function(err, data){
+    //find tweets
+    var tweet = data.statuses;
+    var randomTweet = ranDom(tweet); //pick a random tweet
+
+    //if random exists
+    if(typeof randomTweet != 'undefined'){
+      //tell twiiter to favorite
+      Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
+        //if there was an error while 'favorite'
+        if(err){
+          console.log('cannot be favorite ...error');
+        }
+        else{
+          console.log('favorited ... success!!!');
+        }
+      });
+    }
+  });
+}
+
+//grab & favorite
+favoriteTweet();
+
+//favorite a tweet in every 60 mins
+setInterval(favoriteTweet, 3000000);
+
+//function to generate a random tweet tweet
+function ranDom(arr){
+  var index = Math.floor(Math.random()*arr.length);
+  return arr[index];
+}
